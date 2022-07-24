d9de503e8accd22fc2c6c2ac4c91b60b79972a2c
fgenu <fpgenu@gmail.com>
2022-07-24 19:49:59 -0300

    add to permissions class require user id method

--- src/java/controller/Permissions.java
+++ src/java/controller/Permissions.java
@@ -20,8 +20,18 @@ public class Permissions {
             throw new PermissionDeniedException("Permissões insuficientes para realizar a operação.");
         }
     }
+
+    static void requireUserId(HttpServletRequest request, Integer... id) {
+        requireLogin(request);
+        HttpSession session = request.getSession();
+        Funcionario usuario = (Funcionario) session.getAttribute("usuario");
+        boolean allowed = asList(id).contains(usuario.getId());
+        if (!allowed) {
+            throw new PermissionDeniedException("Permissão negada ao usuário; operação não pôde ser realizada.");
+        }
+    }
     
-    static void requireLogin(HttpServletRequest request) {
+    private static void requireLogin(HttpServletRequest request) {
         HttpSession session = request.getSession();
         if(session == null) {
             throw new PermissionDeniedException("Sessão não encontrada; operação não pôde ser realizada.");
