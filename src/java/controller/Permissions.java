d11d9e0f7f1a4fdad4b1988576ceaabcfb67977c
fgenu <fpgenu@gmail.com>
2022-07-24 15:51:57 -0300

    add to permissions class require login method

--- src/java/controller/Permissions.java
+++ src/java/controller/Permissions.java
@@ -3,6 +3,7 @@ package controller;
 import application.Funcionario;
 import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -10,9 +11,23 @@ import javax.servlet.http.HttpServletRequest;
  */
 public class Permissions {
     static void requireRole(String role, HttpServletRequest request) throws IOException {
-        Funcionario usuario = (Funcionario) request.getSession().getAttribute("usuario");
+        requireLogin(request);
+        HttpSession session = request.getSession();
+        Funcionario usuario = (Funcionario) session.getAttribute("usuario");
         if (!usuario.getPapel().equals(role)) {
             throw new PermissionDeniedException("Permissões insuficientes para realizar a operação.");
+        }
+    }
+    
+    static void requireLogin(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        if(session == null) {
+            throw new PermissionDeniedException("Sessão não encontrada; operação não pôde ser realizada.");
+        } else {
+            Funcionario usuario = (Funcionario) session.getAttribute("usuario");
+            if(usuario == null) {
+                throw new PermissionDeniedException("Usuário não encontrado; operação não pôde ser realizada.");
+            }
         }
     }
 }
